0cacb8b0a8ece59dcb05fcdd991cc86b092d5e04
Dylan Praul <dpraul@ionfish.org>
2015-12-10 10:36:59 -0500

    Working client

--- README.md
+++ README.md
@@ -9,6 +9,11 @@ Add nonsense client side encryption for project. Client must enter password on e
 Use "openssl s_client -connect <HOSTNAME>:5000" to connect.
 If connecting to and from a linux lab account, you can just use the server name (eva:5000, avatar:5000, etc).
 
+For JavaScript client, verified cert/key pair must be use. I have those generated from https://letsencrypt.org/ for cpe490.dylanpraul.com. Email me for the cert/key pair, then set hosts file to forward cpe490.dylanpraul.com to 127.0.0.1
+
+## Client install.
+Works in Chrome only. Go to More tools --> Extentions --> Check "Developer mode" --> Click "Load unpacked extension..." --> Navigate to /client/ folder, then click "Launch"
+
 ##Chat-Server
 Simple chatroom in C unsing SSL to tunnel the connection between client and server. The repository is a fork of the [original Chat Server](https://github.com/yorickdewid/Chat-Server "Chat Server") repository. Due to the many requests for a secure version of he chat server I've decided to fork the project into a new repository. This project demonstrates the basic use of sockets and SSL. There is currently no client available but any telnet client will do. However I have planned a SSL client as well for the future. For now, just connect to the server on the specified port and address. By default port 5000 is used. The project was intended to run on Linux and Unix based systems. However with minor changes you'd be able to run it on Windows as well.
 
--- client/client.js
+++ client/client.js
@@ -1,53 +1,59 @@
 window.socketId;
 window.tcp = chrome.sockets.tcp;
 window.socketProperties = {
-	"persistent": true,
-	"name": "Chatroom"
+	//"persistent": true,
+	//"name": "Chatroom"
+};
+window.secureOptions = {
+	"tlsVersion": {
+		"min": "tls1",
+		"max": "tls1"
+	}
 };
 
 
 function ab2str(buf) {
-  return String.fromCharCode.apply(null, new Uint16Array(buf));
+  return String.fromCharCode.apply(null, new Uint8Array(buf));
 }
+
 function str2ab(str) {
-  var buf = new ArrayBuffer(str.length*2); // 2 bytes for each char
-  var bufView = new Uint16Array(buf);
+  var buf = new ArrayBuffer(str.length * 2); // 2 bytes for each char
+  var bufView = new Uint8Array(buf);
   for (var i=0, strLen=str.length; i < strLen; i++) {
     bufView[i] = str.charCodeAt(i);
   }
   return buf;
 }
 
-tcp.onReceive.addListener(function(info) {
+function onReceive(info) {
 	console.log(info);
 	if (info.socketId != socketId) {
 		return;
 	}
-});
+	log(ab2str(info.data));
+}
+
+
+function log(contents) {
+	var output = document.getElementById("output");
+	output.innerHTML = output.innerHTML + "<br>" + contents;
+}
 
 tcp.create(socketProperties, function(newSocketInfo) {
 	socketId = newSocketInfo.socketId;
 });
 
-function log(contents) {
-	var output = document.getElementById("output");
-	console.log(contents);
-	output.innerHTML = output.innerHTML + "<br>" + contents;
-}
-
 function connect() {
-	tcp.connect(socketId, "127.0.0.1", 5000, function (result) {
-	});
-}
-
-function secure() {
-	tcp.secure(socketId, {
-		"tlsVersion": {
-			"min": "tls1",
-			"max": "tls1.2"
-		}
-	}, function(result) {
-		console.log(result);
+	tcp.onReceive.addListener(onReceive);
+	tcp.setPaused(socketId, true, function() {
+		var server = document.getElementById("server").value;
+		var port = parseInt(document.getElementById("port").value);
+		tcp.connect(socketId, server, port, function () {
+			tcp.secure(socketId, secureOptions, function() {
+				tcp.setPaused(socketId, false, function() {
+				});
+			});
+		});
 	});
 }
 
@@ -56,9 +62,9 @@ function send() {
 	tcp.send(socketId, str2ab(input.value), function(resultCode, bytesSent) {
 		console.log(resultCode);
 	});
-	input.value = 0;
+	log(input.value);
+	input.value = "";
 }
 
 document.getElementById('connect').onclick = connect;
-document.getElementById('secure').onclick = secure;
 document.getElementById('send').onclick = send;
